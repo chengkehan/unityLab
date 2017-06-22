c42f69a87f5edc1f5ed7ecb34d7c7cdd6663d67f
chengkehan <chengkehan@gmail.com>
2017-06-22 18:09:58 +0800

    test code

--- KDopTree/Assets/KDopTree.cs
+++ KDopTree/Assets/KDopTree.cs
@@ -10,6 +10,8 @@ public class KDopTree
 
     public const float KINDA_SMALL_NUMBER = 0.0001f;
 
+    public const int NUM_PLANES = 3;
+
     public List<KDopNode> nodes = null;
 
     public List<KDopTriangle> triangles = null;
@@ -21,17 +23,11 @@ public class KDopTree
             return;
         }
 
-        if(nodes == null)
+        if(nodes != null)
         {
-            nodes = new List<KDopNode>();
+            nodes.Clear();
         }
-        nodes.Clear();
-
-        if (triangles == null)
-        {
-            triangles = new List<KDopTriangle>();
-        }
-        triangles.Clear();
+        nodes = new List<KDopNode>();
 
         BuildTriangles(mesh);
 
@@ -42,11 +38,25 @@ public class KDopTree
 
     private void BuildTriangles(Mesh mesh)
     {
+        List<int[]> trisList = new List<int[]>();
+        int totalTris = 0;
+        for (int meshIndex = 0; meshIndex < mesh.subMeshCount; ++meshIndex)
+        {
+            int[] tris = mesh.GetTriangles(meshIndex);
+            trisList.Add(tris);
+            totalTris += tris.Length / 3;
+        }
+
+        if(triangles != null)
+        {
+            triangles.Clear();
+        }
+        triangles = new List<KDopTriangle>(totalTris);
+
         Vector3[] vertices = mesh.vertices;
         for(int meshIndex = 0; meshIndex < mesh.subMeshCount; ++meshIndex)
         {
             int[] tris = mesh.GetTriangles(meshIndex);
-
             for (int triIndex = 0; triIndex < tris.Length; triIndex += 3)
             {
                 KDopTriangle tri;
@@ -61,6 +71,8 @@ public class KDopTree
                 }
             }
         }
+
+
     }
 }
 
@@ -74,7 +86,67 @@ public struct KDopNode
 
     public void SplitTriangleList(int startTriangleIndex, int numTriangles, KDopTree kDopTree)
     {
+        if(numTriangles > 4)
+        {
+            isLeaf = false;
 
+            int BestPlane = -1;
+            float BestMean = 0.0f;
+            float BestVariance = 0.0f;
+            for (int nPlane = 0; nPlane < KDopTree.NUM_PLANES; nPlane++)
+            {
+                float Mean = 0.0f;
+                float Variance = 0.0f;
+                for (int nTriangle = startTriangleIndex; nTriangle < startTriangleIndex + numTriangles; nTriangle++)
+                {
+                    Mean += kDopTree.triangles[nTriangle].GetCentroid()[nPlane];
+                }
+                Mean /= (float)numTriangles;
+                for (int nTriangle = startTriangleIndex; nTriangle < startTriangleIndex + numTriangles; nTriangle++)
+                {
+                    float Dot = kDopTree.triangles[nTriangle].GetCentroid()[nPlane];
+                    Variance += (Dot - Mean) * (Dot - Mean);
+                }
+                Variance /= (float)numTriangles;
+                if (Variance >= BestVariance)
+                {
+                    BestPlane = nPlane;
+                    BestVariance = Variance;
+                    BestMean = Mean;
+                }
+            }
+
+            int Left = startTriangleIndex - 1;
+            int Right = startTriangleIndex + numTriangles;
+            while (Left < Right)
+            {
+                float Dot;
+                do
+                {
+                    Dot = kDopTree.triangles[++Left].GetCentroid()[BestPlane];
+                }
+                while (Dot < BestMean && Left < Right);
+                do
+                {
+                    Dot = kDopTree.triangles[--Right].GetCentroid()[BestPlane];
+                }
+                while (Dot >= BestMean && Right > 0 && Left < Right);
+                if (Left < Right)
+                {
+                    KDopTriangle Temp = kDopTree.triangles[Left];
+                    kDopTree.triangles[Left] = kDopTree.triangles[Right];
+                    kDopTree.triangles[Right] = Temp;
+                }
+            }
+            if (Left == startTriangleIndex + numTriangles || Right == startTriangleIndex)
+            {
+                Left = startTriangleIndex + (numTriangles / 2);
+            }
+        }
+        else
+        {
+            isLeaf = true;
+        }
     }
 }
 
